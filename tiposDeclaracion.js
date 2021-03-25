c1260895fbf27610764c2f1eb116598f8c6b6f28
paubernalq <bernalq.pau@gmail.com>
2021-03-24 19:43:04 -0600

    Subiendo archivos por primera vez

---
+++ tiposDeclaracion.js
@@ -0,0 +1,36 @@
+    //Programa número 1 en JavaScript
+
+ //Declaración de Variable
+    var numero = 46;
+
+        console.log( numero );
+    //para cambiar el valor de una variable
+        numero = 455;
+        console.log( numero );
+
+ // Declaración de Constante
+    const mensaje = "Bienvenido a Luxemburgo";
+    
+    //si quisieramos cambiar una constate (imposible)
+    // mensaje = "Bienvenido a Ciudad de México";
+    console.log( mensaje );
+
+//Características de persona
+    const nombre = "Paulina";
+    const apellidoPaterno = "Villareal";
+    const apellidoMaterno = "Villaseñor";
+    var edad = "16";
+    const lugarNacimiento = "México";
+
+//Ejercicio: Define las declaraciones para
+//5 características de un coche
+
+    var placa = "ABC-2575";
+    const marca = "jetta";
+    const modelo = "small";
+    const paisOrigen = "Hawaii";
+    const anio = "2005";
+ 
+let numero5 = 39;
+
+
